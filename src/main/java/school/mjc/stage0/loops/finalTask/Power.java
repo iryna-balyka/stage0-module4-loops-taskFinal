d282ffaa15b9f88eb076f7d4674f82e816cb53a3
Ирина <irina.kanus3@gmail.com>
2023-02-22 18:24:48 +0400

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.finalTask;
 public class Power {
     public void printPower(int numberToPrint, int power) {
         int result = 1;
-        for (int i = 1; i < power; i++) {
+        for (int i = 0; i < power; i++) {
             result = result * numberToPrint;
         }
         System.out.println(result);
